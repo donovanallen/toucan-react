59563bf005f59e7b684d2dc79820f821a1705b8c
Donovan Allen <dallen.928@gmail.com>
2022-12-06 17:33:40 -0600

    fixes landing page rsp

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -20,44 +20,42 @@ const marqueeList = [
 
 const LandingPage = () => {
 	return (
-		<div className="app-container">
-			<div className="app-content">
-				<header className="app-header">
-					<div className="hero-heading-main">
-						<h1 className="hero-heading-text">
-							donovan<span className="hero-heading-text-landmark">allen</span>
-						</h1>
-					</div>
-					<Typed
-						className="hero-marquee-text"
-						strings={marqueeList}
-						typeSpeed={60}
-						backDelay={2000}
-						backSpeed={30}
-						cursorChar={'_'}
-						shuffle
-						loop
-					></Typed>
-				</header>
-				<div className="app-content-text">
-					<p>
-						currently: creating digital elevated experiences at{' '}
-						<a href="https://shop.lululemon.com" target="_blank">
-							lululemon
-						</a>
-					</p>
-					<p>
-						previously: software engineer at{' '}
-						<a
-							className="secondary"
-							href="https://www.deloittedigital.com/"
-							target="_blank"
-						>
-							Deloitte Digital
-						</a>
-					</p>
-					{/* <p>this site was built with react</p> */}
-					{/* <p>
+		<div className="app-content">
+			<header className="app-header">
+				<h1 className="hero-heading-text">
+					donovan<span className="hero-heading-text-landmark">allen</span>
+				</h1>
+				<Typed
+					className="hero-marquee-text"
+					strings={marqueeList}
+					typeSpeed={60}
+					backDelay={2000}
+					backSpeed={30}
+					cursorChar={'_'}
+					shuffle
+					loop
+				></Typed>
+			</header>
+			<div className="app-content-text">
+				<p>
+					currently: <br /> creating digital elevated experiences at{' '}
+					<a href="https://shop.lululemon.com" target="_blank">
+						lululemon
+					</a>
+				</p>
+				<p>
+					previously: <br />
+					software engineer at{' '}
+					<a
+						className="secondary"
+						href="https://www.deloittedigital.com/"
+						target="_blank"
+					>
+						Deloitte Digital
+					</a>
+				</p>
+				{/* <p>this site was built with react</p> */}
+				{/* <p>
             "Whatever our role, we are designers of information. Our choices
             alter the presentation and flow of human knowledge. We control how
             people find, understand, and use information in every facet of their
@@ -68,7 +66,6 @@ const LandingPage = () => {
             - Lisa Marie Marquis, "Everyday Information Architecture" (2019)
           </p>
           <ArrowDown className="content-arrow-down" size={36} strokeWidth={1} /> */}
-				</div>
 			</div>
 		</div>
 	);
