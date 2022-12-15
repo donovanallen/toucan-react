de864a43f8f5b8905cce29c36c8175b4eced3f1b
Donovan Allen <dallen.928@gmail.com>
2022-12-14 20:49:17 -0600

    Dnvnhaus (#45)
    * adds logo and animation
    
    * fixes favicon

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,10 +1,24 @@
 import './nav-bar.scss';
+import { useState } from 'react';
+import dnvn from '../../assets/logo-main.png';
 
 const NavBar = () => {
+	const [hovered, setHovered] = useState(false);
+	const toggleHover = () => setHovered(!hovered);
+
 	return (
 		<nav className="nav-bar">
-			<div className="logo">
+			<div
+				className="logo"
+				onMouseEnter={toggleHover}
+				onMouseLeave={toggleHover}
+			>
 				<a href="/">da</a>
+				{hovered && (
+					<a href="https://dnvn.haus" className={hovered ? 'show' : 'hide'}>
+						<img src={dnvn} alt="dnvn-logo" />
+					</a>
+				)}
 			</div>
 			<div className="nav-links">
 				<a href="about">about</a>
